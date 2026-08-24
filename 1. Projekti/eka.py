6f026fb8668aabd7a843fc99a8219c00b43bab97
Provoda <provodochek2@gmail.com>
2026-08-24 15:06:56 +0300

    Disable project tracking and debug script

--- 1. Projekti/eka.py
+++ 1. Projekti/eka.py
@@ -1,10 +1,5 @@
 pi = 3.141596
-sade = 4.0
+sade = 4
 
 
-print("1. Ympyrän piiri on:")
-print(2*pi*sade)
-print("\n2. Ympyrän pinta-ala on: ")
-print(pi*sade**2)
-print("\n3. Ympyrän halkaisija on: ")
-print(2*sade)
+print(type(sade))

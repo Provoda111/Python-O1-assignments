9fb9c7480f80e17b73342288d23d773290898e31
Provoda <provodochek2@gmail.com>
2026-08-24 14:55:52 +0300

    Add project files and move program.py

---
+++ 1. Projekti/README.md
@@ -0,0 +1,8 @@
+# Otsikko
+
+<span style='color: red'>Tämä on minun **ensimmäinen** projekti</span>
+
+### Alaotsikko
+<font color="red">Tässä yksi kaava:</font>
+
+$\pi r^2$
---
+++ 1. Projekti/eka.py
@@ -0,0 +1,10 @@
+pi = 3.141596
+sade = 4.0
+
+
+print("1. Ympyrän piiri on:")
+print(2*pi*sade)
+print("\n2. Ympyrän pinta-ala on: ")
+print(pi*sade**2)
+print("\n3. Ympyrän halkaisija on: ")
+print(2*sade)
